26e470554c9e2f81693eba7e3b4d8486a2f09c4d
Karishma Keshav <karishma.keshav@gmail.com>
2026-09-07 16:05:47 -0700

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_int_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res = 0
+        for i in range(len(s)):
+            if i+1 < len(s) and roman_int_map[s[i]] < roman_int_map[s[i+1]]:
+                res -= roman_int_map[s[i]]
+            else:
+                res+=roman_int_map[s[i]]
+        return res
+        
